6a2a2998c150a1c6b2438f0c2fe211804f983477
Tom van Etten <tomvanetten@outlook.com>
2019-02-28 16:05:50 +0100

    init

--- cli_mesh/Debug/configPkg/package/cfg/release_pem4f.cfg.mak
+++
@@ -1,8 +0,0 @@
-# invoke SourceDir generated makefile for release.pem4f
-release.pem4f: .libraries,release.pem4f
-.libraries,release.pem4f: package/cfg/release_pem4f.xdl
-	$(MAKE) -f C:\Users\TOMVAN~1\workspace_v8\cli_ftd_CC26X2R1_LAUNCHXL_tirtos_ccs/src/makefile.libs
-
-clean::
-	$(MAKE) -f C:\Users\TOMVAN~1\workspace_v8\cli_ftd_CC26X2R1_LAUNCHXL_tirtos_ccs/src/makefile.libs clean
-
--- cli_mesh/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibopenthread_diag_ftd.lib -llibopenthread_cli_ftd.lib -llibopenthread_ftd.lib -llibopenthread_platform_utils_ftd.lib -llibmbedcrypto.lib -l"ti/display/lib/display.aem4f" -l"ti/grlib/lib/ccs/m4f/grlib.a" -l"third_party/spiffs/lib/ccs/m4f/spiffs_cc26xx.a" -l"ti/drivers/rf/lib/rf_multiMode_cc26x2.aem4f" -l"ti/drivers/lib/drivers_cc26x2.aem4f" -l"ti/dpl/lib/dpl_cc26x2.aem4f" -l"ti/devices/cc13x2_cc26x2/driverlib/bin/ccs/driverlib.lib" -llibc.a
-
