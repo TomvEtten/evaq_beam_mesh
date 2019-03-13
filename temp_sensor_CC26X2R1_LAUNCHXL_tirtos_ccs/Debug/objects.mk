762a30e496c746f447726ccfac824b64784e34ab
Ralph Stoffers <ralphstoffers@gmail.com>
2019-03-13 16:15:35 +0100

    remove debug

--- temp_sensor_CC26X2R1_LAUNCHXL_tirtos_ccs/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibopenthread_diag_mtd.lib -llibopenthread_mtd.lib -llibopenthread_platform_utils_mtd.lib -llibmbedcrypto.lib -l"ti/display/lib/display.aem4f" -l"ti/grlib/lib/ccs/m4f/grlib.a" -l"third_party/spiffs/lib/ccs/m4f/spiffs_cc26xx.a" -l"ti/drivers/rf/lib/rf_multiMode_cc26x2.aem4f" -l"ti/drivers/lib/drivers_cc26x2.aem4f" -l"ti/dpl/lib/dpl_cc26x2.aem4f" -l"ti/devices/cc13x2_cc26x2/driverlib/bin/ccs/driverlib.lib" -llibc.a
-
